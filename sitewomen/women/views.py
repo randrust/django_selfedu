4b05641678b4ded1b637c42a72b7094b3f3d6535
Andrii Rakutskyi <randrur@gmail.com>
2023-09-01 14:13:05 +0300

    Add paths

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Main page women")
+
+def categories(request):
+    return HttpResponse("<h1>List Categories</h1>")
