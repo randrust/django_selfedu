7a601a2d67e9d3ab32e74e40a8e546f05763180e
Andrii Rakutskyi <randrur@gmail.com>
2023-09-10 12:40:18 +0300

    Add template tag url

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -6,10 +6,14 @@ register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about', views.about, name='about'),
-    path('cats/<int:cat_id>/', views.categories, name='cat_id'),
-    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),
-    path('archive/<year4:year>/', views.archive, name='archive'),
+    path('about/', views.about, name='about'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
+    path('addaticle/', views.addaticle, name='add_aticle'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='log_in'),
+    # path('cats/<int:cat_id>/', views.categories, name='cat_id'),
+    # path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),
+    # path('archive/<year4:year>/', views.archive, name='archive'),
 ]
 
 
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -5,7 +5,12 @@ from django.urls import reverse
 
 # Create your views here.
 
-menu = ['About Site', 'Add aticle', 'Contact', 'Log in']
+menu = [
+    {'title': 'About Site', 'url_name': 'about'},
+    {'title': 'Add aticle', 'url_name': 'add_aticle'},
+    {'title': 'Contact', 'url_name': 'contact'},
+    {'title': 'Log in', 'url_name': 'log_in'},
+    ]
 
 data_db = [
     {'id': 1, 'title': 'A. Djolly', 'content': 'About A. Djolly', 'is_published': True},
@@ -24,19 +29,32 @@ def about(request):
     return render(request, "women/about.html", {'title': 'About Site'})
 
 
-def categories(request, cat_id):
-    return HttpResponse(f"<h1>List Categories</h1><p>id: {cat_id}")
+def show_post(request, post_id):
+    return HttpResponse(f'View post with id: {post_id}')
+
+def addaticle(request):
+    return HttpResponse('Add atcile')
+
+def contact(request):
+    return HttpResponse('Contact')
+
+def login(request):
+    return HttpResponse('Log in')
 
 
-def categories_by_slug(request, cat_slug):
-    return HttpResponse(f"<h1>List Categories</h1><p>slug: {cat_slug}")
+# def categories(request, cat_id):
+#     return HttpResponse(f"<h1>List Categories</h1><p>id: {cat_id}")
 
 
-def archive(request, year):
-    if year > 2023:
-        uri = reverse('cats', args=('sport',))
-        return redirect(uri)
-    return HttpResponse(f"<h1>List By Year</h1><p>slug: {year}")
+# def categories_by_slug(request, cat_slug):
+#     return HttpResponse(f"<h1>List Categories</h1><p>slug: {cat_slug}")
+
+
+# def archive(request, year):
+#     if year > 2023:
+#         uri = reverse('cats', args=('sport',))
+#         return redirect(uri)
+#     return HttpResponse(f"<h1>List By Year</h1><p>slug: {year}")
 
 
 def page_not_found(request, exception):
